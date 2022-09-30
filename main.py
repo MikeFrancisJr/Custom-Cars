95162cbe43df7f0e3c111857a8b9063768afbf88
MikeFrancisJr <114704008+MikeFrancisJr@users.noreply.github.com>
2022-09-30 00:42:21 +0000

    displayed 3 engine types

--- main.py
+++ main.py
@@ -1,5 +1,47 @@
+# Tell the user what type of form this is
 print("=======================")
 print("Welcome to the UMBC")
 print ("Car Customization Form")
 print("=======================")
 print()
+# Display a message asking the user to pick a letter for multiple choice questions 
+print("(For multiple choice problems, please enter the letter of the selection")
+print("you're looking for ~ Make & Model ~)")
+print()
+# Dislpay a message asking the user what car model they want to buy
+print("1. What Car Model are you interested in purchasing?")
+# Display a list of four options labeled 'a' through 'd'
+print("  a. Toyota Camry")
+print("  b. Jeep Cherokee")
+print("  c. Acura TLX")
+print("  d. Range Rover")
+# Ask the user to enter a, b, c, or d
+carmodel = input("Please enter 'a' - 'd': ")
+print()
+# Display a message asking the user would they like all wheel drive
+print("2. Do you prefer All-Wheel Drive?")
+# Ask the user to enter 'yes' or 'no'
+driveType = input("Please enter 'yes' or 'no': ")
+print()
+# Display the message '~Exterior~'
+print("~ Exterior ~")
+print()
+# Display a message asking the user what color car they want
+print("3. What is your ideal car color?")
+# Ask the user to enter the color
+carColor = input("Please enter your ideal color: ")
+print()
+# Display a message asking the user if they would like the deluxe weather package
+print("4. Would you like the deluxe weather package?")
+# Ask the user to enter 'yes' or 'no'
+package = input("Please enter 'yes' or 'no': ")
+print()
+# Display the message ~Interior~
+print("~ Interior ~")
+# Display a message asking the user what type of engine do they want
+print("5. What type of engine would you like your car to have?")
+# Display a list of 3 options labeled 'a' - 'c'
+print("  a. I-4 Entry Engine")
+print("  b. V-6 Performance Engine")
+print("  c. Eco Diesel Engine")
+# Ask the user to enter a, b, or c
